0905196fac4778cb5ff12bdaa2617a6616cb1ee7
ShreyanshRaj22 <211230051@nitdelhi.ac.in>
2025-05-24 23:57:08 +0530

    User Auth for websocket complete

--- backend/middlewares/socketAuth.js
+++ backend/middlewares/socketAuth.js
@@ -1,21 +1,21 @@
 const cookie = require('cookie');
 const jwt = require('jsonwebtoken');
-const { getUserById } = require('../db/data-providers/user.provider'); // your data-provider
+// const { getUserById } = require('../db/data-providers/user.provider'); // your data-provider
 
 const socketAuth = async (socket, next) => {
     try {
         const cookies = socket.handshake.headers.cookie;
         if (!cookies) return next(new Error('No cookies provided'));
-
         const parsedCookies = cookie.parse(cookies);
         const token = parsedCookies.accessToken;
         if (!token) return next(new Error('Access token missing'));
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await getUserById(decoded.id);
-        if (!user) return next(new Error('User not found'));
+        console.log("decoded:===> ", decoded);
+        // const user = await getUserById(decoded.userId);
+        // if (!user) return next(new Error('User not found'));
 
-        socket.user = user; // attach user to socket
+        socket.user = decoded.userId;
         next();
     } catch (err) {
         next(new Error('Unauthorized: ' + err.message));
--- backend/websockets/handlers/vote.handler.js
+++ backend/websockets/handlers/vote.handler.js
@@ -2,8 +2,8 @@ const { handleVote } = require('../../services/feed/vote.service');
 const { emitToPollRoom } = require('../utils/socketUtils');
 
 module.exports = (io, socket) => {
-    socket.on('vote', async ({ pollId, optionId, userId }) => {
-        const updatedPoll = await handleVote(pollId, optionId, userId);
+    socket.on('vote', async ({ pollId, optionId }) => {
+        const updatedPoll = await handleVote(pollId, optionId, socket.user);
 
         // Notify all users viewing this poll
         emitToPollRoom(io, pollId, 'pollUpdate', updatedPoll);
